9e015bb94f2e83f660e3cb8d13f5c90eca3becb0
Ivan Claudio <ivan.claudio@gmail.com>
2022-11-23 14:13:15 -0300

    Troubleshooting code

--- build/run.sh
+++ build/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 echo "${MAVEN_SETTINGS}" > /code/maven-settings.xml
-mvn -s /code/maven-settings.xml clean package -DskipTests ${EXTRA_ARGS}
+mvn -s /code/maven-settings.xml clean package -DskipTests ${EXTRA_ARGS} -X
