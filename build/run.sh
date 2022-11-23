7bb87025c631af3ad27480eaba2da0dcc12c67b0
Ivan Claudio <ivan.claudio@gmail.com>
2022-11-23 16:32:06 -0300

    Added upload to Nexus

--- build/run.sh
+++ build/run.sh
@@ -2,3 +2,4 @@
 set -e
 echo "${MAVEN_SETTINGS}" > /code/maven-settings.xml
 mvn -s /code/maven-settings.xml clean package -DskipTests ${EXTRA_ARGS}
+mvn -s /code/maven-settings.xml deploy -DskipTests ${EXTRA_ARGS}
