467fbf53429e6456dc3da2d62357d4fc36756746
Ivan Claudio <ivan.claudio@gmail.com>
2022-11-24 11:14:55 -0300

    Troubleshooting action deploy

--- deploy-k8s/deploy-k8s.sh
+++ deploy-k8s/deploy-k8s.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-echo "${K8S_CONFIG}" > k8sconfig.yaml
+echo "${K8S_CONFIG}" > actionsTools/deploy-k8s/k8sconfig.yaml
 export KUBECONFIG=k8sconfig.yaml
 kubectl apply -f ${MANIFEST} -n ${NAMESPACE}
